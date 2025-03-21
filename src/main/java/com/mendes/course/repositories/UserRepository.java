c332156847c7a09c7ed0e23d2c8a4547a4714f1d
juliostmendes <mendes.julio@pm.me>
2025-03-21 17:57:04 -0300

    Service layer, component registration

--- src/main/java/com/mendes/course/repositories/UserRepository.java
+++ src/main/java/com/mendes/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.mendes.course.repositories;
 
 import com.mendes.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+// Using the annotation @Repository in this case is optional,
+// because this interface already extends a JpaRepository that
+// is a Spring component
 public interface UserRepository extends JpaRepository<User, Long> {
 }
