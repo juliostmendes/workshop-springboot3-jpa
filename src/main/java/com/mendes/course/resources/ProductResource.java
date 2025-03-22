171fff9188a2e514b82b5621befcfbcd0e24b92f
juliostmendes <mendes.julio@pm.me>
2025-03-22 16:25:03 -0300

    Products

---
+++ src/main/java/com/mendes/course/resources/ProductResource.java
@@ -0,0 +1,32 @@
+package com.mendes.course.resources;
+
+import com.mendes.course.entities.Product;
+import com.mendes.course.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll(){
+        List list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
