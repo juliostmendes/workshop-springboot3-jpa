1773eb2981f178c69e07117f295371f8b80a619c
juliostmendes <mendes.julio@pm.me>
2025-03-25 18:33:23 -0300

    Exception handling - delete

--- src/main/java/com/mendes/course/services/UserService.java
+++ src/main/java/com/mendes/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.mendes.course.services;
 
 import com.mendes.course.entities.User;
 import com.mendes.course.repositories.UserRepository;
+import com.mendes.course.services.exceptions.DatabaseException;
 import com.mendes.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
