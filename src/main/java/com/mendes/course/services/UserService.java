8cc4cfd6844faecd7acf3381e4a14a6a7b638e08
juliostmendes <mendes.julio@pm.me>
2025-03-25 17:08:49 -0300

    Exception handling - findById

--- src/main/java/com/mendes/course/services/UserService.java
+++ src/main/java/com/mendes/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.mendes.course.services;
 
 import com.mendes.course.entities.User;
 import com.mendes.course.repositories.UserRepository;
+import com.mendes.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
